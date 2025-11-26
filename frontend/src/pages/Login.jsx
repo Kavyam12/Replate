64f8ec44ccd159ead37061772036c159d47b616a
Tummula Kavyam Kumar <Kavyamkumart2004@gmail.com>
2025-11-26 10:42:11 +0530

    Made changes for login and registration pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,100 @@
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import "./Login.css"
+
+// function Login() {
+//   const navigate = useNavigate();
+
+//   const redirectToHome = () => {
+//     navigate('/home');
+//   };
+//   return (
+//     <div>
+//       <div className="login-box">
+//         <div className="login-header">
+//           <header>Login</header>
+//         </div>
+
+//         <div className="input-box">
+//           <input type="text" className="input-field" placeholder="Email" autoComplete="off" required />
+//         </div>
+
+//         <div className="input-box">
+//           <input type="password" className="input-field" placeholder="Password" autoComplete="off" required />
+//         </div>
+
+//         <div className="forgot">
+//           <section>
+//             <input type="checkbox" id="check" />
+//             <label htmlFor="check">Remember me</label>
+//           </section>
+//         </div>
+
+//         <div className="input-submit">
+//           <button className="btn-17" onClick={redirectToHome}>
+//             <span className="text-container">
+//               <span className="text">Submit</span>
+//             </span>
+//           </button>
+//         </div>
+
+//         <div className="sign-up-link">
+//           <p>Don't have account? <a href="/registration">Sign Up</a></p>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default Login
+
 import React from 'react'
+import "./Login.css"
+import { useNavigate } from 'react-router-dom'
 
 function Login() {
+
+  const navigate =  useNavigate();
+
+  const redirectToHome = () => {
+    navigate("/");
+  };
   return (
-    <div>login</div>
+    <div>
+
+      <div className="login-box">
+
+        <div className="login-header">
+          <header>Login</header> 
+        </div>
+
+        <div className="input-field">
+          <input type="text" className='input-field' placeholder='Email' autoComplete='off' required />
+        </div>
+
+        <div className="input-field">
+          <input type="password" className='input-field' placeholder='Password' autoComplete='off' required />
+        </div>
+
+        <div className="remember-me">
+          <section>
+            <input type="checkbox" id='check' />
+            <label htmlFor="check">Remember me</label>
+          </section>
+        </div>
+
+        <div className="submit-btn">
+          <buttoo className="btn-submit" onClick = {redirectToHome} >
+            <span className="text">
+              <span className="text-inside">
+                Submit
+              </span>
+            </span>
+          </buttoo>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import "./Login.css"
 
 function Registration() {
+
+  const navigate = useNavigate();
+
+  const redirectToLogin = () => {
+    navigate("/login");
+  };
+
   return (
-    <div>Registration</div>
+    <div>
+      <div className="login-box">
+
+        <div className="login-header">
+          <header>Registration</header>
+        </div>
+
+        <div className="input-field">
+          <input type="text" className='input-field' placeholder='Full Name' autoComplete='off' required />
+        </div>
+
+        <div className="input-field">
+          <input type="email" className='input-field' placeholder='Email' autoComplete='off' required />
+        </div>
+
+        <div className="input-field">
+          <input type="password" className='input-field' placeholder='Password' autoComplete='off' required />
+        </div>
+
+        <div className="input-field">
+          <input type="password" className='input-field' placeholder='Confirm Password' autoComplete='off' required />
+        </div>
+
+        <div className="input-field">
+          <input type="dropdown" className='roles' placeholder = 'select role' autoComplete='off' required />
+        </div>
+
+        <div className="submit-btn">
+          <button className="btn-submit">
+            <span className="text">
+              <span className="text-inside">
+                Register
+              </span>
+            </span>
+          </button>
+        </div>
+
+        <div className="sign-up-link">
+          <p>Already have an account? <a onClick={redirectToLogin} style={{cursor:'pointer'}}>Login</a></p>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
